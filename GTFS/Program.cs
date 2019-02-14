fda6fc7480e568f6aef134ff2dd0c358284ceda1
Rez <rezghelich@gmail.com>
2019-02-14 16:26:43 +1300

    first version of csv reader

---
+++ GTFS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
+
+
+namespace GTFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (CsvReader file =
+                new CsvReader(new StreamReader(@"C:\Users\rez\Desktop\WGTNgoogle-transit\agency.txt"), true))
+                {
+                    int fieldCount = file.FieldCount;
+
+                    string[] headers = file.GetFieldHeaders();
+
+                    while (file.ReadNextRecord())
+                    {
+                        for (int i = 0; i < fieldCount; i++)
+                            Console.Write(string.Format("{0 } = {1 };", headers[i], file[i]));
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.ReadKey();
+            }
+
+            catch
+            {
+
+            }
+        }
+    }
+}
